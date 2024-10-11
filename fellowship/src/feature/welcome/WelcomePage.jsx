245f4f142439379f6077ea6444c65a8a2085529e
Abdul_Ghafur <jdoe@inlichtingenbureau.nl>
2024-10-11 22:45:21 +0200

    ....

--- fellowship/src/feature/welcome/WelcomePage.jsx
+++ fellowship/src/feature/welcome/WelcomePage.jsx
@@ -87,7 +87,7 @@ const WelcomePage = () => {
                 </div>
               </section>
               <section>
-                dd
+                dll
               </section>
             </form>
 
